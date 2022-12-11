321c3522b6c0251a2bb39b1e244f16804841d919
Akshay <akshayjee6@gmail.com>
2022-12-11 12:39:28 +0530

    Redirected Routes

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Product() {
   return (
     <div>
       <h1>Product Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="products/p2"> A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A Dog</Link>
+        </li>
+      </ul>
     </div>
   );
 }
