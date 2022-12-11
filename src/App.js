ebc5a75eb0f863886009a73f266182eb33ca879a
Akshay <akshayjee6@gmail.com>
2022-12-11 12:11:03 +0530

    Added Link in place of anchor tags

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
 import Product from "./pages/Product";
 import Welcome from "./pages/Welcome";
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/product">
-        <Product />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+      </main>
     </div>
   );
 }
